8765829cbd5c55a21daaba4b2d64c6a86f8a9a63
susantamohapatra-creditsaison <susanta.mohapatra@creditsaison-in.com>
2025-11-20 12:42:40 +0000

    consolidated pipeline changes

--- SampleOrdersPipeline/transformations/OrdersDeltaLiveTables.sql
+++ SampleOrdersPipeline/transformations/OrdersDeltaLiveTables.sql
@@ -1,26 +1,37 @@
-CREATE OR REFRESH STREAMING LIVE TABLE books_bronze
-  COMMENT "The raw books data, ingested from CDC feed"
-  AS SELECT * FROM cloud_files("${datasets.path}/books-cdc", "json");
+CREATE OR REFRESH STREAMING LIVE TABLE orders_raw
+COMMENT "The raw books orders, ingested from orders-raw"
+AS SELECT * FROM cloud_files("${datasets.path}/orders-json-raw", "json",
+                             map("cloudFiles.inferColumnTypes", "true"));
 
-CREATE OR REFRESH STREAMING LIVE TABLE books_silver;
+CREATE OR REFRESH LIVE TABLE customers
+COMMENT "The customers lookup table, ingested from customers-json"
+AS SELECT * FROM json.`${datasets.path}/customers-json`;
 
-APPLY CHANGES INTO LIVE.books_silver
-  FROM STREAM(LIVE.books_bronze)
-  KEYS (book_id)
-  APPLY AS DELETE WHEN row_status = "DELETE"
-  SEQUENCE BY row_time
-  COLUMNS * EXCEPT (row_status, row_time);
+CREATE OR REFRESH STREAMING LIVE TABLE orders_cleaned (
+  CONSTRAINT valid_order_number EXPECT (order_id IS NOT NULL) ON VIOLATION DROP ROW
+)
+COMMENT "The cleaned books orders with valid order_id"
+AS
+  SELECT order_id, quantity, o.customer_id, c.profile:first_name as f_name, c.profile:last_name as l_name,
+         cast(from_unixtime(order_timestamp, 'yyyy-MM-dd HH:mm:ss') AS timestamp) order_timestamp, o.books,
+         c.profile:address:country as country
+  FROM STREAM(LIVE.orders_raw) o
+  LEFT JOIN LIVE.customers c
+    ON o.customer_id = c.customer_id;
 
-CREATE LIVE TABLE author_counts_state
-  COMMENT "Number of books per author"
-  AS SELECT author, count(*) as books_count, current_timestamp() updated_time
-    FROM LIVE.books_silver
-    GROUP BY author;
+CREATE OR REFRESH LIVE TABLE cn_daily_customer_books
+COMMENT "Daily number of books per customer in China"
+AS
+  SELECT customer_id, f_name, l_name, date_trunc("DD", order_timestamp) order_date, sum(quantity) books_counts
+  FROM LIVE.orders_cleaned
+  WHERE country = "China"
+  GROUP BY customer_id, f_name, l_name, date_trunc("DD", order_timestamp);
 
-CREATE LIVE VIEW books_sales
-  AS SELECT b.title, o.quantity
-    FROM (
-      SELECT *, explode(books) AS book 
-      FROM LIVE.orders_cleaned) o
-    INNER JOIN LIVE.books_silver b
-    ON o.book.book_id = b.book_id;
+CREATE OR REFRESH LIVE TABLE fr_daily_customer_books
+COMMENT "Daily number of books per customer in France"
+AS
+  SELECT customer_id, f_name, l_name, date_trunc("DD", order_timestamp) order_date, sum(quantity) books_counts
+  FROM LIVE.orders_cleaned
+  WHERE country = "France"
+  GROUP BY customer_id, f_name, l_name, date_trunc("DD", order_timestamp);
+
